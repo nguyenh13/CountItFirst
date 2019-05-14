9455c824b885e9ec599c3ba053f54aa3cc5933ce
Hung Nguyen <nguyenh13@spu.edu>
2019-05-14 15:57:18 -0700

    "Updated Algorithm"

---
+++ test-algo.py
@@ -0,0 +1,24 @@
+import random
+
+ranNum = random.randint(10,30)
+print(ranNum)
+sum = 0
+while sum < ranNum:
+    addNum =-1
+    while addNum < 1 or addNum > 2:
+        for j in range(0,3):
+            print("PLAYER ",j," TURN")
+            addNum = int(input("Add 1 or 2? "))
+            sum += addNum
+            if sum <= ranNum:
+                print("Current number: ",sum)
+                print("\n")
+            if sum == ranNum or sum == ranNum+1:
+                print("You win")
+                break
+            for i in range(1,3):
+                if sum == ranNum-i :
+                    print("Let's win it")
+                
+
+
